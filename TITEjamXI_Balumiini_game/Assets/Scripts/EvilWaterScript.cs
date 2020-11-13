20a2ff0069636c32c945786bceb5912565704a2a
tstkokki <60537602+tstkokki@users.noreply.github.com>
2020-11-13 22:47:01 +0200

    watershader(WIP)

---
+++ TITEjamXI_Balumiini_game/Assets/Scripts/EvilWaterScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EvilWaterScript : MonoBehaviour
+{
+    Vector3 rising = new Vector3(0, 0.2f, 0);
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        transform.Translate(rising*Time.deltaTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
