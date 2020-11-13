6ca9e989d234aab50d073f60cef0a9efe011548d
tstkokki <60537602+tstkokki@users.noreply.github.com>
2020-11-13 22:24:58 +0200

    prefabs folder and plartform

---
+++ TITEjamXI_Balumiini_game/Assets/Scripts/ObjectPooler.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPooler : MonoBehaviour
+{
+    public static ObjectPooler PoolInstance;
+    public List<GameObject> poolList;
+    public GameObject pooledObject;
+    public int poolAmount;
+
+    private void Awake()
+    {
+        PoolInstance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        poolList = new List<GameObject>();
+        for(int i = 0; i < poolAmount; i++)
+        {
+            GameObject obj = Instantiate(pooledObject);
+            obj.SetActive(false);
+            poolList.Add(obj);
+        }
+    }
+
+    public GameObject GetPooledObjecT()
+    {
+        for (int i = 0;i < poolList.Count; i++)
+        {
+            if (!poolList[i].activeInHierarchy)
+            {
+                return poolList[i];
+            }
+        }
+        return null;
+    }
+}
