88b5243845a2742ac3b5bbd7416491022ca35d49
tstkokki <60537602+tstkokki@users.noreply.github.com>
2020-11-14 13:10:13 +0200

    masterTimer

--- TITEjamXI_Balumiini_game/Assets/Scripts/UI_Master_Script.cs
+++ TITEjamXI_Balumiini_game/Assets/Scripts/UI_Master_Script.cs
@@ -9,23 +9,56 @@ public class UI_Master_Script : MonoBehaviour
     public float curHeight = 0f;
     private float step = 0f;
     private float wallStep = 0f;
+    public float playTime = 0f;
     public TextMeshProUGUI heightText;
+    public TextMeshProUGUI timeText;
     private Transform Player;
 
+    [Header("Masters")]
     public Platform_Master_Script platMaster;
     public Wall_Master_Script wallMaster;
+    public EvilWaterScript evilWater;
 
+
+
+    public enum WaterState
+    {
+        Low = 0,
+        Intermediate = 1,
+        Hard = 2,
+        GettingMoist = 3,
+        Splashy = 4,
+        BathroomLeakage = 5,
+        Flood = 6,
+        Tsunami = 7
+    }
+    public WaterState curTide = WaterState.Low;
     // Start is called before the first frame update
     void Start()
     {
         Player = FindObjectOfType<Player_Controller_Script>().gameObject.transform;
         platMaster = FindObjectOfType<Platform_Master_Script>();
         wallMaster = FindObjectOfType<Wall_Master_Script>();
+        evilWater = FindObjectOfType<EvilWaterScript>();
+    }
+
+    private void FixedUpdate()
+    {
+        CalcTime();
     }
 
     private void LateUpdate()
     {
         CalcPlayerHeight();
+    }
+
+    void CalcTime()
+    {
+        playTime += Time.fixedDeltaTime;
+        if(curTide == WaterState.Low && playTime >= 15f)
+        {
+
+        }
     }
 
     void CalcPlayerHeight()
