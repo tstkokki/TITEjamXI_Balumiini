d560a52193ce6ca2d41236269aacca0501187c6b
tstkokki <60537602+tstkokki@users.noreply.github.com>
2020-11-15 19:07:39 +0200

    build versio

---
+++ TITEjamXI_Balumiini_game/Assets/IdleSugarScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IdleSugarScript : MonoBehaviour
+{
+    Animator sugarani;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sugarani = GetComponent<Animator>();
+        sugarani.Play("metarig|Mainmenu", 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- TITEjamXI_Balumiini_game/Assets/Scripts/SceneController.cs
+++ TITEjamXI_Balumiini_game/Assets/Scripts/SceneController.cs
@@ -2,21 +2,45 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using UnityEngine.UI;
 public class SceneController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Animator doorLeft;
+    public Animator doorRight;
+    public Button creditsButton;
+    public GameObject creditsPanel;
+    bool isTransitioning;
 
+    public void ToggleCredits()
+    {
+        creditsPanel.SetActive(!creditsPanel.activeSelf);
+    }
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && !isTransitioning)
         {
-            SceneManager.LoadScene("Harton_scene");
+            StartGame();
         }
+
+        if (Input.GetKeyDown(KeyCode.Backspace))
+        {
+            Application.Quit();
+        }
+    }
+
+    public void StartGame()
+    {
+        isTransitioning = true;
+        doorLeft.Play("DoorLeft");
+        doorRight.Play("DoorRight");
+        StartCoroutine("SwapScene");
+        //SceneManager.LoadScene("Harton_scene");
+    }
+
+    IEnumerator SwapScene()
+    {
+        yield return new WaitForSeconds(2.1f);
+        SceneManager.LoadScene(1);
     }
 }
