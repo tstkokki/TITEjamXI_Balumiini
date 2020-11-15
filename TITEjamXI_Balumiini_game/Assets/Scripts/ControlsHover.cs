3f588f20523f90dbdf6d729c0fe7c833f292b91d
ciini004 <ciini004@edu.xamk.fi>
2020-11-15 15:18:10 +0200

    cartoonkitchen

---
+++ TITEjamXI_Balumiini_game/Assets/Scripts/ControlsHover.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlsHover : MonoBehaviour
+{
+    public GameObject ControlsText;
+    void Start()
+    {
+        ControlsText.SetActive(false);
+    }
+
+    public void OnMouseEnter()
+    {
+        ControlsText.SetActive(true);
+    }
+
+    public void OnMouseExit()
+    {
+        ControlsText.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
